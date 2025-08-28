88050d17b709a83b8f4f5068f11683af335b67e7
Godfred48 <godfredagbogah48@gmail.com>
2025-08-28 00:32:40 +0000

    y

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./component/Home/home";
 import AuthPage from "./component/Auth";
-import {BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import {HashRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
