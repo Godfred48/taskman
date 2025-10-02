85fb09682c6b4399e387ef5e49ff7c3fdcf798cb
Godfred48 <godfredagbogah48@gmail.com>
2025-10-02 11:50:09 +0000

    Dashboard Updated

--- my_react_app/src/component/Dashboard/header.jsx
+++ my_react_app/src/component/Dashboard/header.jsx
@@ -1,17 +1,27 @@
 import icon from "../images/tasklogo.svg";
-import logout from "../images/logout.svg";
 import "../style/Dashboard.css";
-
+import { useState } from 'react';
+ 
 const Header = () => {
+        const [sidebarOpen, setsidebarOpen] = useState(true)
+        const handlesidebarButton =()=>{
+                setsidebarOpen(!sidebarOpen)
+        }
 
     return (
             <header className="top-nav">
-                    <img src={icon} alt="logo" className="dashboard-logo"/>
-                    <span><h2>Dashboard</h2></span>
-                    <button className="logout-button">
-                         <img src={logout} alt="logout" />
-                         <span><p>Logout</p></span>
-                    </button>
+                <img src={icon} alt="logo" className="dashboard-logo"/>
+                <span><h2>Dashboard</h2></span>
+                <section className="sidebarButton" onClick={handlesidebarButton}>   
+                        {sidebarOpen ? ( 
+                         <>
+                                <span>☰</span>
+                        </>
+                                )
+                        :(
+                                <span>✖</span>   
+                        )}      
+                </section>
             </header>
      );
 }
