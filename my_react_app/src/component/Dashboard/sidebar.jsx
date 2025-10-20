900a0847b8810eb1d26b80f0b714a6a76cbdab7a
Godfred48 <godfredagbogah48@gmail.com>
2025-10-20 17:28:40 +0000

    update o f nav and side bar

--- my_react_app/src/component/Dashboard/sidebar.jsx
+++ my_react_app/src/component/Dashboard/sidebar.jsx
@@ -1,14 +1,19 @@
 import "../style/Dashboard.css";
 import {Link} from "react-router-dom";
+import home from "../images/house.svg";
+import dash from "../images/dash.svg";
+import Logout from "../images/logout.svg";
+import settings from "../images/settings.svg";
+import task from "../images/list-task.svg";
 const Sidebar = ({logout}) => {
     return (  
         <aside className="sidebar">
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/taskitem">Tasks</Link></li>
-                <li><Link to="/settings">Setting</Link></li>
-                <li onClick={logout}><Link to="/login">Logout</Link></li>
+                <li><Link to="/"><img src={home} alt="home"/>Home</Link></li>
+                <li><Link to="/dashboard"><img src={dash} alt="dashboard"/>Dashboard</Link></li>
+                <li><Link to="/taskitem"><img src={task} alt="task" />Tasks</Link></li>
+                <li><Link to="/settings"><img src={settings} alt="setting"/>Setting</Link></li>
+                <li onClick={logout}><Link to="/login"><img src={Logout} alt="logout"/>Logout</Link></li>
             </ul>
         </aside>
     );
--- my_react_app/src/component/Dashboard/smsidebar.jsx
+++ my_react_app/src/component/Dashboard/smsidebar.jsx
@@ -1,5 +1,11 @@
 import { motion, AnimatePresence } from "framer-motion";
+import { Link } from "react-router-dom";
 import "../style/Dashboard.css";
+import home from "../images/house.svg";
+import dash from "../images/dash.svg";
+import Logout from "../images/logout.svg";
+import settings from "../images/settings.svg";
+import task from "../images/list-task.svg";
 
 const Smsidebar = ({ sidebarOpen,logout }) => {
   return (
@@ -12,13 +18,13 @@ const Smsidebar = ({ sidebarOpen,logout }) => {
           exit={{ x: "-100%" }}          // slides out
           transition={{ duration: 0.3, ease: "easeInOut" }} // smooth motion
         >
-          <ul>
-            <li>Home</li>
-            <li>Dashboard</li>
-            <li>Tasks</li>
-            <li>Settings</li>
-            <li onClick={logout}>Logout</li>
-          </ul>
+           <ul>
+                <li><Link to="/"><img src={home} alt="home"/>Home</Link></li>
+                <li><Link to="/dashboard"><img src={dash} alt="dashboard"/>Dashboard</Link></li>
+                <li><Link to="/taskitem"><img src={task} alt="task" />Tasks</Link></li>
+                <li><Link to="/settings"><img src={settings} alt="setting"/>Setting</Link></li>
+                <li onClick={logout}><Link to="/login"><img src={Logout} alt="logout"/>Logout</Link></li>
+            </ul>
         </motion.aside>
       )}
     </AnimatePresence>
